4663b7850bce82a2269fdee041d0ce995a4c3d81
unullmass <codemanure@live.co.uk>
2015-10-24 17:53:46 -0400

    Fixed logic. Search loop breaks after finding 2nd factor. Now also generates a composite,least prime factor pair file. Can be plotted to for data analysis.

--- primes.rb
+++ primes.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/ruby
 
 primefile = ARGV[0] ? ARGV[0] : "myprimes.list"
-numprimesreqd = ARGV[1] ? ARGV[1].to_i : 1000
+primefactorsfile = ARGV[1] ? ARGV[1] : "myprimefactors.list"
+numprimesreqd = ARGV[2] ? ARGV[2].to_i : 10000
 
 primelist = []
+primefactorslist = []
+
 
 i = 0
 if File.exist? primefile
@@ -21,13 +24,13 @@ primes_gen_so_far = 0
 
 until primes_gen_so_far == numprimesreqd
 	num_of_prime_factors = 0
+	my_prime_factor = 1
 	primelist.each do | myprime |
-		if num_of_prime_factors > 2 and myprime > (cur_prime / 2)
+		if num_of_prime_factors == 2 or myprime > (cur_prime / 2)
 			break			
-		else
-			if cur_prime % myprime == 0
-				num_of_prime_factors += 1
-			end
+		elsif cur_prime % myprime == 0
+			num_of_prime_factors += 1
+			my_prime_factor = myprime
 		end
 	end
 
@@ -35,13 +38,24 @@ until primes_gen_so_far == numprimesreqd
 		primelist[primelist.length] = cur_prime
 		puts cur_prime.to_s + " is prime"
 		primes_gen_so_far += 1
+	else
+		if cur_prime % 2 != 0 
+			primefactorslist[primefactorslist.length] = cur_prime.to_s + "," + my_prime_factor.to_s
+		end
 	end
+
 	cur_prime += 1
 end
 
 File.open(primefile, "w") do |file|
 	primelist.each do |myprime| 
 		file.puts myprime
+	end
+end
+
+File.open(primefactorsfile, "a") do |file|
+	primefactorslist.each do |myprimefactor| 
+		file.puts myprimefactor
 	end
 end
 
